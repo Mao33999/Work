b4d0a8e5a93761710f24c42c8ef3845a0ada9a66
Mao33999 <landofbp@gmail.com>
2025-02-10 15:14:57 +0300

    Update Decency.java

--- src/Decency.java
+++ src/Decency.java
@@ -1,4 +1,96 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Decency {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your equation: ");
+        String s = scanner.nextLine();
+        String expression = s.replaceAll(" ", "");
+        System.out.println(parse(expression));
+    }
+    public static String parse(String expression) throws Exception {
+        int num1;
+        int num2;
+        String oper;
+        String result;
+        boolean isRoman;
+        String[] operands = expression.split("[+\\-*/]");
+        if (operands.length !=2) throw new Exception("Supposed to be two operands");
+        oper = detectOperation(expression);
+        if(Roman.isRoman(operands[0]) && Roman.isRoman(operands[1])) {
+            num1 = Roman.convertToArabian(operands[0]);
+            num2 = Roman.convertToArabian(operands[1]);
+            isRoman = true;
+        }
+        else if (!Roman.isRoman(operands[0]) && !Roman.isRoman(operands[1])) {
+            num1 = Integer.parseInt(operands[0]);
+            num2 = Integer.parseInt(operands[1]);
+            isRoman = false;
+        }
+        else {
+            throw new Exception("Numbers should be in a same format");
+        }
+        if (num1 > 10 || num2 > 10) {
+            throw new Exception("Numbers should be 1 to 10");
+        }
+        int arabian = calc(num1, num2, oper);
+        if (isRoman) {
+            if (arabian <= 0) {
+                throw new Exception("Roman numeral should be higher than 0");
+            }
+            result = Roman.convertToRoman(arabian);
+        } else {
+            result = String.valueOf(arabian);
+        }
+        return result;
+    }
+    class Roman {
+        static String[] romanArray = new String[]{
+                "0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+                "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII",
+                "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI",
+                "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX", "LXI",
+                "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV",
+                "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI",
+                "LXXXVII", "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
+
+        public static String convertToRoman(int arabian) {
+            if (arabian < 1 || arabian > 100) {
+                throw new IllegalArgumentException("Result out of range for Roman numerals (should be between 1 and 100)");
+            }
+            return romanArray[arabian];
+        }
+        public static boolean isRoman(String str) {
+            for (int i = 0; i < romanArray.length; i++) {
+                if (str.equals(romanArray[i])) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static int convertToArabian(String roman) {
+            for (int i = 0; i < romanArray.length; i++) {
+                if (roman.equals(romanArray[i])) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+    static String detectOperation(String expression) {
+        if (expression.contains("+")) return "+";
+        else if (expression.contains("-")) return "-";
+        else if (expression.contains("*")) return "*";
+        else if (expression.contains("/")) return "/";
+        else return null;
+    }
+    static int calc(int a, int b, String oper) {
+        if (oper.equals("+")) return a + b;
+        else if (oper.equals("-")) return a - b;
+        else if (oper.equals("*")) return a * b;
+        else return a / b;
+    }
+
 }
